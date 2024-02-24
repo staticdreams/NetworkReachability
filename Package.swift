b19ad37f83d26e227629f5703b6a6eaf5444f13e
Peter <kernelpanic@protonmail.com>
2024-02-24 22:34:48 +0100

    fixed typo

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .iOS(.v11),
         .macOS(.v10_13),
         .tvOS(.v11),
-        .watchOS(.v5)
+        .watchOS(.v5),
         .visionOS(.v1)
     ],
     products: [
